f22d999a29115c7b8828cc3886b4db0a03b8d0f9
Yochran <71285258+Yochran@users.noreply.github.com>
2021-03-11 13:56:20 -0500

    Create 14.java

---
+++ 14.java
@@ -0,0 +1,9 @@
+public static boolean trueRandom() {
+  int first = new Random().nextInt(10);int second = new Random().nextInt(10);int third = new Random().nextInt(10);
+  int fourth = new Random().nextInt(10);int fifth = new Random().nextInt(10);int sixth = new Random().nextInt(10);int seventh = new Random().nextInt(10);
+  if(first<second&&third%fourth==fifth&&true&&(sixth>>seventh==1)&&true&&true&&true) {
+    return first<second&&third%fourth==fifth&&true&&(sixth>>seventh==1)&&true&&true&&true;
+  } else {
+    return false;
+  }
+}
