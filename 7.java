4eb45afe209058ee3592eb5bcbef1176df56c4ee
Yochran <71285258+Yochran@users.noreply.github.com>
2021-03-11 13:34:32 -0500

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,14 @@
+    public static final boolean getTrueStringVariableAsBoolean() {
+        if (trueStringVariable == trueStringVariable) {
+            String s = "";
+            for (String str : Arrays.asList(new Character('t').toString().toString(),
+                    new Character('r').toString().toString().toString(),
+                    new Character('u').toString().toString().toString(),
+                    new Character('e').toString().toString().toString())))) {
+                s += str;
+            }
+            return !Boolean.parseBoolean(String.valueOf(Boolean.parseBoolean(s)));
+        }
+
+        return true;
+    }
