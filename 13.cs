d6bdb52d34bf8313c56e3cddf2031a51f82d54c8
Yochran <71285258+Yochran@users.noreply.github.com>
2021-03-11 13:52:01 -0500

    Create 13.cs

---
+++ 13.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace 12 {
+  class 12 {
+    static void Main(String[] args) 
+    {
+      while (((((true))))) 
+      {
+        Console.ForegroundColor = ConsoleColor.BLACK;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        Console.WriteLine(a);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        Console.ForegroundColor = ConsoleColor.White;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 
+; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 
+; ; ; ; ; ; ; ; 
+      }
+
+      if (true == false) {{{{{{{{{{{{{{{{{{
+        true = false;;;; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 
+      }}}}}}}}}}}}}}}}}
+    }
+  }    
+}
