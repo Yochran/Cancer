6955669a90fcd7e124853598ce253dcae2d3ee86
Yochran <71285258+Yochran@users.noreply.github.com>
2021-03-11 13:42:13 -0500

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,24 @@
+#include <stlib.h>
+#include <iostream>
+#include <string>
+
+class 10 {
+  public:
+    void _void() {
+      {
+        {
+          {
+            {
+              {
+                {
+                  {
+                    void 1() { void 2() { void 3() { void 4() { std::cout << "void" << std::endl; std::string __void = "void" } } } }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }      
+}
