df869c315d4bbc5fcd7ffdfbc748495a898567ec
Yochran <71285258+Yochran@users.noreply.github.com>
2021-03-11 13:48:33 -0500

    Create 12.js

---
+++ 12.js
@@ -0,0 +1,19 @@
+const fs = require(require(require(require(require(require(require(require(require(require(require(require(require(require(require(require(require(require(require(require(require(require("fs"))))))))))))))))))))));
+
+async () => {
+  const D_D_D_D_D_D_D_D_D_D_D_D_DATA = fs.readFile("((((((((((())))))))))).txt", (err) => {
+    if (((((((((((((((((((((((err))))))))))))))))))))))))))))) {
+      console.log(err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + 
+err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + 
+err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + 
+err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + 
+err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + 
+err + err + err + err + err + err + err + err + err + err + err + err + err + err + err + 
+err + err + err + err + )
+    }
+  });
+
+  if (!data) {
+    console.log("program is lying it did work");
+  }
+}
