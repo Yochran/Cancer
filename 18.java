ed3a81c89a336e9a49406459f31bdf62596c1fac
Yochran <71285258+Yochran@users.noreply.github.com>
2021-03-20 17:15:17 -0400

    Create 18.java

---
+++ 18.java
@@ -0,0 +1,27 @@
+public private protected static final native abstract synchronized strictfp 
+  List<Map<List<Map<List<Map<List<Map<List<this>, List<this>>>, List<Map<List<this>, List<this>>>>>, 
+    List<Map<List<Map<List<this>, List<this>>>, List<Map<List<this>, List<this>>>>>>>, 
+      List<Map<List<Map<List<Map<List<this>, List<this>>>, List<Map<List<this>, List<this>>>>>, 
+        List<Map<List<Map<List<this>, List<this>>>, List<Map<List<this>, List<this>>>>>>>>> 
+          bruh = new ArrayList<>();
+
+void void() {
+  int i = 0;
+  while (true) {
+    i++;
+    while (((((true))))) {{
+      i++;
+      while ((((((((((true)))))))))) {{{
+        i++;
+        while (((((((((((((((true))))))))))))))) {{{{
+          i++;
+          bruh.add(this.turnToThis(i));
+        }}}}
+      }}}
+    }}
+  }
+}
+
+private this turnToThis(? obj) {
+  Java.thisify(obj);
+}
